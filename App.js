d09b8acacea1eb7909967148ac2a3c31fcc71b73
obatlle <batlle.or@gmail.com>
2018-09-16 16:12:56 +0200

    Add Roboto fonts

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
+import { Font, AppLoading } from 'expo';
+
 
 import * as reducers from './app/reducers';
 import RootStackNavigator from './app/modules/RootStackNavigator';
@@ -13,14 +15,43 @@ const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const reducer = combineReducers(reducers);
 const store = createStoreWithMiddleware(reducer);
 
+function cacheFonts(fonts) {
+    return fonts.map(font => Font.loadAsync(font));
+}
 
 
 export default class Index extends Component {
 
-
-  componentDidMount(){
+  constructor() {
+      super();
+      this.state = {
+          isReady: false,
+      }
   }
+
+  async _loadAssetsAsync() {
+      const fontAssets = cacheFonts([
+          {RobotoExtraBold: require('./app/assets/fonts/Roboto-Black.ttf')},
+          {RobotoBold: require('./app/assets/fonts/Roboto-Bold.ttf')},
+          {RobotoMedium: require('./app/assets/fonts/Roboto-Medium.ttf')},
+          {RobotoRegular: require('./app/assets/fonts/Roboto-Regular.ttf')},
+          {RobotoLight: require('./app/assets/fonts/Roboto-Light.ttf')}
+      ]);
+
+      await Promise.all([...fontAssets]);
+  }
+
   render() {
+    if (!this.state.isReady) {
+        return (
+            <AppLoading
+                startAsync={this._loadAssetsAsync}
+                onFinish={() => this.setState({isReady: true})}
+                onError={console.warn}
+            />
+        );
+    }
+
     return (
       <View
        style={{flex:1}}>
